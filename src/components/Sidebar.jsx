37fe8f1a78f1156f3bed099bf6864fbf0f18e913
Zach Taylor <124469454+ZachTaylor2002@users.noreply.github.com>
2024-08-14 10:48:26 -0500

    Added 'Your Library' section with icons and layout styling.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -14,6 +14,18 @@ const Sidebar = () => {
                 <p className='font-bold'>Search</p>
             </div>
         </div>
+        <div className='bg-[#121212] h-[85%] rounded'>
+          <div className='p-4 flex items-center justify-between'>
+            <div className='flex items-center gap-3'>
+                <img className='w-8' src={assets.stack_icon}/>
+                <p className='font-semibold'>Your Library</p>
+            </div>
+              <div className='flex items-center gap-3'>
+                  <img className='w-5' src={assets.arrow_icon} alt=""/>
+                  <img className='w-5' src={assets.plus_icon} alt=""/>
+              </div> 
+          </div>
+        </div>
     </div>
   )
 }
